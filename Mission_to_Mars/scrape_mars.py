1dc87df2cc8f21c2fd24b9a3cdc142573448f6fa
darrenlefort <63559435+darrenlefort@users.noreply.github.com>
2020-09-12 14:21:05 -0400

    dictionary link corrections

--- Mission_to_Mars/scrape_mars.py
+++ Mission_to_Mars/scrape_mars.py
@@ -74,7 +74,7 @@ def scrape():
         "news_title": news_title,
         "news_p": news_p,
         "featured_image_url": featured_image_url,
-        "mars_weather": mars_weather,
+        # "mars_weather": mars_weather,
         "fact_table": str(mars_html_table),
         "hemisphere_images": hemisphere_image_urls
     }
